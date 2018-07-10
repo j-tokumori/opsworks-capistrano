f096c9085810e59c3e089c4191e4db4e103f5379
Junya Tokumori <j-tokumori@bns-g.com>
2018-07-10 18:57:16 +0900

    ssh_config cap タスク追加

--- config/deploy/meta.rb
+++ config/deploy/meta.rb
@@ -7,11 +7,12 @@ task :populate => :extinguish do
   stages = Hash.new { {} }
 
   opsworks.describe_stacks.stacks.each do |stack|
+    # puts stack.custom_json
     stages[stack.name] = {}
     opsworks.describe_layers(:stack_id => stack.stack_id).layers.each do |layer|
       opsworks.describe_instances(:layer_id => layer.layer_id).instances.reject { |instance| instance.public_ip.nil? }.each do |instance|
         # stages[stack.name][instance.public_ip] ||= { :user => instance.os.include?("Amazon Linux") ? "ec2-user" : "ubuntu", :roles => [] }
-        stages[stack.name][instance.public_ip] ||= { :host => instance.hostname, :roles => [] }
+        stages[stack.name][instance.public_ip] ||= { :roles => [] }
         stages[stack.name][instance.public_ip][:roles] << layer.name
       end
     end
@@ -33,5 +34,27 @@ desc "Remove the results of :populate"
 task :extinguish do
   (Dir["config/deploy/*.rb"] - [__FILE__]).each do |stage|
     File.delete stage
+  end
+end
+
+desc "todo"
+task :ssh_config do
+  opsworks = Aws::OpsWorks::Client.new
+  output_path = ENV['OUTPUT_PATH'] || File.join(Dir.home, '.ssh/conf.d/hosts')
+
+  opsworks.describe_stacks.stacks.each do |stack|
+    File.open(File.join(output_path, stack.name), "w") do |file|
+      file.puts "# #{stack.name}"
+      file.puts
+      opsworks.describe_layers(:stack_id => stack.stack_id).layers.each do |layer|
+        file.puts "## #{layer.name}"
+        opsworks.describe_instances(:layer_id => layer.layer_id).instances.reject { |instance| instance.public_ip.nil? }.each do |instance|
+          file.puts "Host #{instance.hostname}"
+          file.puts "  HostName #{instance.public_ip}"
+        end
+        file.puts
+      end
+    end
+    puts "output: #{File.join(output_path, stack.name)}"
   end
 end
